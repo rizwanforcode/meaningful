743d54465ac0d5b00976091e28829c945fcc3671
Rizwan Ahmad <rizwanforcode@gmail.com>
2022-11-02 16:21:58 +0545

    Added the searchbar right in the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import { BsSearch } from "react-icons/bs";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="container">
+      <form class="search-form">
+        <div className="input-group">
+          <input
+            className="search-field"
+            type="text"
+            placeholder="Enter a word or group of words"
+          />
+          <button type="submit" className="search-icon-wrapper">
+            <BsSearch className="search-icon" />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
